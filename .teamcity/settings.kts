4ac74a314f554efa85481a83ae4fe2b8e755f636
admin <admin@TeamCity>
2021-06-10 10:49:44 +0000

    TeamCity change in '.Net Application CI/CD' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -55,8 +55,8 @@ object Build : BuildType({
             name = "Push to Octopus"
             type = "octopus.push.package"
             param("octopus_space_name", "Default")
-            param("octopus_host", "http://18.205.176.81:8080")
-            param("octopus_packagepaths", "+:./build => %repository%.%build.number%.zip")
+            param("octopus_host", "http://3.236.126.63:8080")
+            param("octopus_packagepaths", "+:./build => ci-cd.zip")
             param("octopus_publishartifacts", "true")
             param("octopus_forcepush", "false")
             param("secure:octopus_apikey", "credentialsJSON:4b9d6efe-8fff-4d80-b4f6-5b2f5699ae9b")
